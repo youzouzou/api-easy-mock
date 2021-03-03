a0626391b740d28c34296ba5b598bc8ab6be45a3
youguifang <guifang.you@tcl.com>
2021-03-03 18:11:29 +0800

    匹配用户自定义接口

--- app.js
+++ app.js
@@ -5,11 +5,23 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api');
 require('./util/file')
 
 var app = express();
+
+function getJSONList() {
+  const JSONUtil = require('./util/file');
+  const jsonFileList = JSONUtil.getFileList("./json/");
+  const jsonList = []
+  jsonFileList.map(item => {
+    const json = JSONUtil.getJson("./json/" + item);
+    if (json.code == 200) {
+      jsonList.push(json.data);
+    }
+  });
+  return jsonList;
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +34,41 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/api', apiRouter);
+
+// 用户请求的自定义api将在这里重新匹配
+app.use(function (req, res) {
+  const jsonList = getJSONList();
+  jsonList.map(item => {
+    console.log(item.api, req._parsedUrl.pathname)
+    console.log(item.method, req.method)
+    if (item.api == req._parsedUrl.pathname && item.method.toUpperCase() == req.method.toUpperCase()) { // api和method匹配到
+      if (item.responseList && item.responseList.length > 0) { // 有定义响应数组
+        let response = null;
+        item.responseList.map(paramAndResObj => {
+          const params = paramAndResObj.data;
+          // 将定义中的参数与请求参数作比较，只有请求参数值与定义的都全部一样，才返回对应结果
+          let isMatch = true;
+          for (key in params) {
+            if (params[key] != req.query[key]) {
+              isMatch = false;
+              break;
+            }
+            console.log(params[key]);
+          }
+          if (isMatch) {
+            response = paramAndResObj.response;
+          }
+        })
+        res.send(response);
+      } else {
+        res.send(JSON.stringify({ val: 222 }));
+      }
+
+    }
+  })
+  res.send("未找到该方法");
+})
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
