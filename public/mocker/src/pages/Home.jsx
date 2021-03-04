e4b8a33713d5a87a64408b3c6fd77f1b42a76306
youguifang <guifang.you@tcl.com>
2021-03-04 10:27:34 +0800

    update

--- public/mocker/src/pages/Home.jsx
+++ public/mocker/src/pages/Home.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import { List, Avatar } from 'antd';
+
+const data = [
+    {
+        title: 'Ant Design Title 1',
+    },
+    {
+        title: 'Ant Design Title 2',
+    },
+    {
+        title: 'Ant Design Title 3',
+    },
+    {
+        title: 'Ant Design Title 4',
+    },
+];
 
 class Home extends React.Component {
     render() {
-        return <h1>Hello</h1>;
+        <List
+            itemLayout="horizontal"
+            dataSource={data}
+            renderItem={item => (
+                <List.Item>
+                    <List.Item.Meta
+                        avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                        title={<a href="https://ant.design">{item.title}</a>}
+                        description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                    />
+                </List.Item>
+            )}
+        />
     }
 }
 
