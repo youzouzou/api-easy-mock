b74c0948767e42edc299734ab4bd2112000b22ef
youguifang <guifang.you@tcl.com>
2021-03-05 18:05:53 +0800

    build && port=3006

--- public/mocker/src/setupProxy.js
+++ public/mocker/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use(
         createProxyMiddleware('/api', {
-            target: 'http://localhost:3001/',
+            target: 'http://localhost:3006/',
             changeOrigin: true,
             pathRewrite: {
                 '^/api': '/mockAPI'
