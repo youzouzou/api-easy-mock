79cf95ade2ed71fe417d35d3f6db646b979631c3
youguifang <guifang.you@tcl.com>
2021-03-05 18:22:48 +0800

    update build

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public/mocker/build')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/mockAPI', apiRouter);
