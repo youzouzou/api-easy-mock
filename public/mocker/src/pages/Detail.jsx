296a0ef33fbfbfcaebd1d68badc021e8385e2f1a
youguifang <guifang.you@tcl.com>
2021-03-05 15:15:24 +0800

    Update Detail.jsx

--- public/mocker/src/pages/Detail.jsx
+++ public/mocker/src/pages/Detail.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 import ReactJson from 'react-json-view';
-import { Form, Input, Button, Select } from 'antd';
+import { Form, Input, Button, Select, Card, Space } from 'antd';
+import { PlusOutlined, MinusCircleOutlined } from '@ant-design/icons';
 const { Option } = Select;
+const { TextArea } = Input;
 
 const Wrapper = styled.div`
     display: flex;
+    .input-wrapper{
+        display:flex;
+    }
+    .add-icon{
+        text-align:right;
+        cursor: pointer;
+    }
 `;
 const FormWrapper = styled.div`
     flex-grow:1;
@@ -32,33 +41,37 @@ class Detail extends React.Component {
             } */
         },
         data: {
-            "api": "/test",
-            "name": "测试接口",
-            "desc": "接口描述",
-            "method": "get",
+            "api": "",
+            "name": "",
+            "desc": "",
+            "method": "GET",
             "headers": {
-                "token": "token值abcdefgxxxxxxxxxxxx"
             },
             "responseList": [
                 {
-                    "data": {
-                        "page": 2
+                    "params": {
+                        "": ""
                     },
-                    "response": {
-                        "返回数据": "12312321"
-                    }
-                },
-                {
-                    "data": {
-                        "page": 1
-                    },
-                    "response": {
-                        "返回数据": "666666"
-                    }
+                    "response": ""
                 }
             ],
-            "defaultResponse": {}
+            "defaultResponse": ""
         }
+    }
+
+    addParam = (index) => () => {
+        console.log(index)
+        const data = this.state.data;
+        const len = Object.keys(data.responseList[index].params).length;
+        data.responseList[index].params["key" + (len + 1)] = ""
+        this.setState({
+            data: data
+        })
+    }
+
+    changeParamKey = (e) => {
+        console.log(e.target.value)
+
     }
 
     onFinish = (values) => {
@@ -69,10 +82,51 @@ class Detail extends React.Component {
         console.log(value)
     }
 
+    validateJSON = async (e, value) => {
+        if (value && !this.isJSON(value)) {
+            return Promise.reject(new Error('JSON格式错误'));
+        }
+    }
+
+    validateResJSON = (e) => {
+        console.log(e.target.value)
+        console.log(this.isJSON(e.target.value))
+    }
+
+    isJSON = (str) => {
+        if (typeof str == 'string') {
+            try {
+                var obj = JSON.parse(str);
+                if (typeof obj == 'object' && obj) {
+                    return true;
+                } else {
+                    return false;
+                }
+
+            } catch (e) {
+                console.log('error：' + str + '!!!' + e);
+                return false;
+            }
+        }
+        return false;
+    }
+
+    addResponse = () => {
+        const data = this.state.data;
+        data.responseList.push({
+            "params": {
+            },
+            "response": ""
+        })
+        this.setState({
+            data: data
+        })
+    }
+
     render() {
         return <Wrapper>
             <FormWrapper>
-                <Form
+                <Form initialValues={this.state.data}
                     name="basic"
                     onFinish={this.onFinish}
                 >
@@ -95,16 +149,60 @@ class Detail extends React.Component {
                     <Form.Item
                         label="请求方法"
                         name="method"
-                        initialValue="GET"
                         required
                     >
-                        <Select defaultValue="GET" onChange={this.handleChange}>
+                        <Select onChange={this.handleChange}>
                             <Option value="GET">GET</Option>
                             <Option value="POST">POST</Option>
                             <Option value="PUT">PUT</Option>
                             <Option value="DELETE">DELETE</Option>
                         </Select>
                     </Form.Item>
+
+                    <Form.Item
+                        label="默认返回结果"
+                        name="defaultResponse"
+                        rules={[{
+                            validator: this.validateJSON,
+                        }]}
+                    >
+                        <TextArea
+                            placeholder="JSON格式的默认返回值"
+                            autoSize={{ minRows: 3, maxRows: 6 }}
+                        />
+                    </Form.Item>
+                    {
+                        this.state.data.responseList.map((res, index) =>
+                            <Card key={index} title={"组合" + (index + 1)}>
+                                <div>
+                                    参数值
+                                </div>
+                                {
+                                    Object.keys(res.params).map((paramkey, _index) =>
+                                        <div className="input-wrapper" key={_index}>
+                                            <Input defaultValue={paramkey} style={{ width: "150px" }} placeholder="参数名称" />
+                                            <Input defaultValue={Object.values(res.params[paramkey])} placeholder="参数值" />
+                                        </div>
+                                    )
+                                }
+                                <div className="add-icon" onClick={this.addParam(index)}><Button block><PlusOutlined />添加请求参数</Button>
+                                </div>
+                                <br />
+                                <div>
+                                    返回值
+                                        <TextArea
+                                        placeholder="JSON格式的返回值"
+                                        autoSize={{ minRows: 3, maxRows: 6 }}
+                                        onChange={this.validateResJSON}
+                                    />
+                                </div>
+                            </Card>
+                        )
+                    }
+
+                    <div onClick={this.addResponse}>
+                        <Button block><PlusOutlined />添加组合</Button>
+                    </div>
 
                     <Form.Item>
                         <Button className="cancel-btn">
